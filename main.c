c87469f4a58ffd8f3c681ebc88d0b823497a8ed1
heesun-hong <dukye2001@naver.com>
2019-10-15 15:35:28 +0900

    06_forth

--- main.c
+++ main.c
@@ -3,41 +3,23 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo(int a, int b)
+
+#include <stdio.h>
+int square(int n);
+
+int main(void)
 {
-	//int output;
-	//output=a+b;
-	return a+b;//output;
+	int result;
+	result= square(5);
+	printf("%d",result);
 }
 
-int square(int n)
+int square (int n)
 {
-	return n*n;
-}
+	return (n*n);
 
-int get_max(int a, int b)
-{
-	if (a > b)
-		return a;
-
-	return b;
 }
+	
 
-int main(int argc, char *argv[]) {
-	
-	int a, b;
-	int output;
-	
-	a=2;
-	b=5;
-	
-	output=sumTwo(a,b);
-	printf("sumTwo : %i\n", output);
-	printf("square : %i\n", square(a));
-	output = get_max(a,b);
-	printf("get_max : %i\n", output);
-	
-	return 0;
-}
 
 
